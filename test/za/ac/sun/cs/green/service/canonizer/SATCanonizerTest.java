ceae1b57f13fe69f3d852f53ccd4f6a4ea115e0e
Emer M. Butler <littleMissMae@users.noreply.github.com>
2018-07-24 11:04:53 +0200

    Test20 LT to LTE

--- test/za/ac/sun/cs/green/service/canonizer/SATCanonizerTest.java
+++ test/za/ac/sun/cs/green/service/canonizer/SATCanonizerTest.java
@@ -40,6 +40,7 @@ public class SATCanonizerTest {
 		String s1 = s0.replaceAll("v[0-9]", "v");
 		SortedSet<String> s2 = new TreeSet<String>(Arrays.asList(s1.split("&&")));
 		SortedSet<String> s3 = new TreeSet<String>(Arrays.asList(expected));
+	
 		assertEquals(s3, s2);
 	}
 
@@ -301,7 +302,7 @@ public class SATCanonizerTest {
 		Operation o1 = new Operation(Operation.Operator.LE, c1, c1);
 		Operation o2 = new Operation(Operation.Operator.LT, v1, c1);
 		Operation o3 = new Operation(Operation.Operator.AND, o1, o2);
-		check(o3, "(2<=2)&&(aa<2)", "1*v+-1<0");
+		check(o3, "(2<=2)&&(aa<2)", "1*v+-1<=0");
 	}
 
 }
