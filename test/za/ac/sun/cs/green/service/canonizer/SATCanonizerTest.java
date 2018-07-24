96d7c5660af2919e101e79d5cab40c764dba2243
Emer M. Butler <littleMissMae@users.noreply.github.com>
2018-07-24 12:34:47 +0200

    Update SATCanonizerTest.java

--- test/za/ac/sun/cs/green/service/canonizer/SATCanonizerTest.java
+++ test/za/ac/sun/cs/green/service/canonizer/SATCanonizerTest.java
@@ -40,7 +40,7 @@ public class SATCanonizerTest {
 		String s1 = s0.replaceAll("v[0-9]", "v");
 		SortedSet<String> s2 = new TreeSet<String>(Arrays.asList(s1.split("&&")));
 		SortedSet<String> s3 = new TreeSet<String>(Arrays.asList(expected));
-		System.out.println("-------\nExpected Output: " + s3.toString() + "\nInput Recieved: " + s2.toString() + "------\n");
+		System.out.println("-------\nExpected Output: " + s3.toString() + "\nInput Recieved: " + s2.toString() + "\n------\n");
 		assertEquals(s3, s2);
 	}
 
