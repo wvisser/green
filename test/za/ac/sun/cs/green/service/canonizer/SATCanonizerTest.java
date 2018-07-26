64cbcfc1303500c61a62e99b8fcbb5737cdb104a
hen3kr8 <32780874+hen3kr8@users.noreply.github.com>
2018-07-26 15:07:44 +0200

    Update SATCanonizerTest.java

--- test/za/ac/sun/cs/green/service/canonizer/SATCanonizerTest.java
+++ test/za/ac/sun/cs/green/service/canonizer/SATCanonizerTest.java
@@ -303,6 +303,7 @@ public class SATCanonizerTest {
 		Operation o3 = new Operation(Operation.Operator.AND, o1, o2);
 		System.out.println("testing123");
 		System.out.println(o3);
+		System.out.println("testing456");
 		check(o3, "(2<=2)&&(aa<2)", "1*v+-1<0");
 	}
 
