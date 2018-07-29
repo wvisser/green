0a85637aefa52b881ce2f9c862b26fe9da5afe99
craigils <C_J_Armstrong@yahoo.com>
2018-07-29 21:36:49 +0200

    Update outputresults.py

--- outputresults.py
+++ outputresults.py
@@ -2,7 +2,7 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('./bin/junit/TEST-za.ac.sun.cs.green.EntireSuite.xml')
 root = tree.getroot()
 
-for test in root.findall("./testsuite/testcase"):
+for test in root.findall("./testsuites/testsuite/testcase"):
     if test.findall("failure"):
         if test.attrib['classname'] == "za.ac.sun.cs.green.service.canonizer.SATCanonizerTest":
             print "SATCanonizerTest: ", test.attrib['name'], " Failed", test.find("failure").attrib['message']
