5a2f06b7e98205d719f82205552cb7587be574cd
Simone-vanZyl <sim.vanzyl@hotmail.com>
2018-07-31 09:52:21 +0200

    Create ConstantPropagation.java

---
+++ src/za/ac/sun/cs/green/service/simplify/ConstantPropagation.java
@@ -0,0 +1,7 @@
+package za.ac.sun.cs.green.service.simplify;
+/* TODO: implemement
+   1. propagating one equality
+   2. adding simplification, which could introduce more equalities to propagate
+   3. do full propagation until you reach a fixpoint
+
+*/
