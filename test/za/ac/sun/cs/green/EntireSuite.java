c2a9e5656b958fe861904137b312c92619faceb7
Sheldon Deacon <20039867@sun.ac.za>
2018-07-31 11:36:19 +0200

    Changed EntireSuite

--- test/za/ac/sun/cs/green/EntireSuite.java
+++ test/za/ac/sun/cs/green/EntireSuite.java
@@ -36,7 +36,9 @@ import za.ac.sun.cs.green.util.SetTaskManagerTest;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
 	SATCanonizerTest.class,
-	SATZ3Test.class
+	SATZ3Test.class,
+	ConstantPropagationTest.class,
+	ConstantPropagationTest2.class
 })
 
 public class EntireSuite {
