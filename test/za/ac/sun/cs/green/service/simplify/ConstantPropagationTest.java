68462e0660272470c4be2dac6479e1759da5b1e6
Sedick David Baker Effendi <20056354@sun.ac.za>
2018-07-31 12:30:02 +0200

    Fixed Willem's spelling mistake

--- test/za/ac/sun/cs/green/service/simplify/ConstantPropagationTest.java
+++ test/za/ac/sun/cs/green/service/simplify/ConstantPropagationTest.java
@@ -28,7 +28,7 @@ public class ConstantPropagationTest {
 		props.setProperty("green.service.sat", "(simplify sink)");
 		//props.setProperty("green.service.sat", "(canonize sink)");
 		props.setProperty("green.service.sat.simplify",
-				"za.ac.sun.cs.green.service.simplify.ConstantPropogation");
+				"za.ac.sun.cs.green.service.simplify.ConstantPropagation");
 		//props.setProperty("green.service.sat.canonize",
 		//		"za.ac.sun.cs.green.service.canonizer.SATCanonizerService");
 		
