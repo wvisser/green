f6a274f3f1305b57cf31d3ba6b27ed6771fe6920
Sedick David Baker Effendi <20056354@sun.ac.za>
2018-07-31 11:39:04 +0200

    First rough test/copy-paste

--- src/za/ac/sun/cs/green/service/simplify/ConstantPropagation.java
+++ src/za/ac/sun/cs/green/service/simplify/ConstantPropagation.java
@@ -1,13 +1,18 @@
 package za.ac.sun.cs.green.service.simplify;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.SortedMap;
 import java.util.SortedSet;
 import java.util.Stack;
 import java.util.TreeMap;
 import java.util.TreeSet;
+import java.util.logging.Level;
 
 import za.ac.sun.cs.green.Green;
+import za.ac.sun.cs.green.Instance;
 import za.ac.sun.cs.green.expr.Constant;
 import za.ac.sun.cs.green.expr.Expression;
 import za.ac.sun.cs.green.expr.IntConstant;
@@ -17,11 +22,61 @@ import za.ac.sun.cs.green.expr.Variable;
 import za.ac.sun.cs.green.expr.Visitor;
 import za.ac.sun.cs.green.expr.VisitorException;
 import za.ac.sun.cs.green.service.BasicService;
+import za.ac.sun.cs.green.util.Reporter;
 
 public class ConstantPropagation extends BasicService {
 	
+	/**
+	 * Number of times the slicer has been invoked.
+	 */
+	private int invocations = 0;
+	
 	public ConstantPropagation(Green solver) {
 		super(solver);
+	}
+	
+	@Override
+	public Set<Instance> processRequest(Instance instance) {
+		@SuppressWarnings("unchecked")
+		Set<Instance> result = (Set<Instance>) instance.getData(getClass());
+		if (result == null) {
+			final Map<Variable, Variable> map = new HashMap<Variable, Variable>();
+			final Expression e = propagate(instance.getFullExpression(), map);
+			final Instance i = new Instance(getSolver(), instance.getSource(), null, e);
+			result = Collections.singleton(i);
+			instance.setData(getClass(), result);
+		}
+		return result;
+	}
+
+	@Override
+	public void report(Reporter reporter) {
+		reporter.report(getClass().getSimpleName(), "invocations = " + invocations);
+	}
+	
+	public Expression propagate(Expression expression,
+			Map<Variable, Variable> map) {
+		try {
+			log.log(Level.FINEST, "Before Canonization: " + expression);
+			invocations++;
+			OrderingVisitor orderingVisitor = new OrderingVisitor();
+			expression.accept(orderingVisitor);
+			expression = orderingVisitor.getExpression();
+			PropagationVisitor canonizationVisitor = new PropagationVisitor();
+			expression.accept(canonizationVisitor);
+			Expression canonized = canonizationVisitor.getExpression();
+			if (canonized != null) {
+				canonized = new Renamer(map,
+						canonizationVisitor.getVariableSet()).rename(canonized);
+			}
+			log.log(Level.FINEST, "After Canonization: " + canonized);
+			return canonized;
+		} catch (VisitorException x) {
+			log.log(Level.SEVERE,
+					"encountered an exception -- this should not be happening!",
+					x);
+		}
+		return null;
 	}
 	
 	private static class OrderingVisitor extends Visitor {
@@ -100,7 +155,7 @@ public class ConstantPropagation extends BasicService {
 
 	}
 
-	private static class CanonizationVisitor extends Visitor {
+	private static class PropagationVisitor extends Visitor {
 
 		private Stack<Expression> stack;
 
@@ -122,7 +177,7 @@ public class ConstantPropagation extends BasicService {
 
 		private boolean linearInteger;
 
-		public CanonizationVisitor() {
+		public PropagationVisitor() {
 			stack = new Stack<Expression>();
 			conjuncts = new TreeSet<Expression>();
 			variableSet = new TreeSet<IntVariable>();
