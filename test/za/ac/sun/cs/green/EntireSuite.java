c7d61c50c49d018c9396b7928e2131d5fd0a1837
19770251 <41615856+19770251@users.noreply.github.com>
2018-07-31 13:15:16 +0200

    Update EntireSuite.java

--- test/za/ac/sun/cs/green/EntireSuite.java
+++ test/za/ac/sun/cs/green/EntireSuite.java
@@ -32,8 +32,8 @@ import za.ac.sun.cs.green.service.z3.SATZ3Test;
 import za.ac.sun.cs.green.util.ParallelSATTest;
 import za.ac.sun.cs.green.util.SetServiceTest;
 import za.ac.sun.cs.green.util.SetTaskManagerTest;
-import za.ac.sun.cs.green.service.simplify.OnlyConstantPropogationTest;
-import za.ac.sun.cs.green.service.simplify.SimplificationConstantPropogationTest;
+import za.ac.sun.cs.green.service.simplify.OnlyConstantPropagationTest;
+import za.ac.sun.cs.green.service.simplify.SimplificationConstantPropagationTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
