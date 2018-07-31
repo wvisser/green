a62ce628380ac28d0eed5e75bf261697e75e3546
Wim Keirsgieter <18187595@sun.ac.za>
2018-07-31 10:39:33 +0200

    Include given propogation tests and skeleton ConstantPropogation file

---
+++ src/za/ac/sun/cs/green/service/simplify/ConstantPropogation.java
@@ -0,0 +1,12 @@
+package za.ac.sun.cs.green.service.simplify;
+
+import za.ac.sun.cs.green.Green;
+import za.ac.sun.cs.green.service.BasicService;
+
+public class ConstantPropogation extends BasicService {
+
+	public ConstantPropogation(Green solver) {
+		super(solver);
+	}
+
+}
