af4cbcac379ec26930a825f589971f8db567020b
Jason Peter Jackson <20131836@sun.ac.za>
2018-07-31 11:22:27 +0200

    new branch and package

---
+++ src/za/ac/sun/cs/green/service/simplify/ConstantPropagation.java
@@ -0,0 +1,18 @@
+package za.ac.sun.cs.green.service.simplify;
+
+import za.ac.sun.cs.green.Instance;
+import za.ac.sun.cs.green.Green;
+import za.ac.sun.cs.green.expr.Expression;
+import za.ac.sun.cs.green.service.BasicService;
+import za.ac.sun.cs.green.util.Reporter;
+import za.ac.sun.cs.green.expr.Constant;
+import za.ac.sun.cs.green.expr.IntConstant;
+import za.ac.sun.cs.green.expr.IntVariable;
+import za.ac.sun.cs.green.expr.Operation;
+import za.ac.sun.cs.green.expr.Variable;
+import za.ac.sun.cs.green.expr.Visitor;
+import za.ac.sun.cs.green.expr.VisitorException;
+
+public class ConstantPropagation extends Visitor{
+	
+}
