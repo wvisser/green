b0eacd6e0c388bae7b213c723f6195987e3f18d0
Simone-vanZyl <sim.vanzyl@hotmail.com>
2018-07-31 12:44:29 +0200

    Update ConstantPropagation.java

--- src/za/ac/sun/cs/green/service/simplify/ConstantPropagation.java
+++ src/za/ac/sun/cs/green/service/simplify/ConstantPropagation.java
@@ -5,3 +5,50 @@ package za.ac.sun.cs.green.service.simplify;
    3. do full propagation until you reach a fixpoint
 
 */
+import za.ac.sun.cs.green.expr.Expression;
+import za.ac.sun.cs.green.service.BasicService;
+import za.ac.sun.cs.green.expr.Constant;
+import za.ac.sun.cs.green.expr.IntConstant;
+import za.ac.sun.cs.green.expr.IntVariable;
+import za.ac.sun.cs.green.expr.Operation;
+import za.ac.sun.cs.green.expr.Variable;
+import za.ac.sun.cs.green.expr.Visitor;
+import za.ac.sun.cs.green.expr.VisitorException;
+import za.ac.sun.cs.green.util.Reporter;
+
+
+public class ConstantPropagation extends BasicService {
+   private int invocations = 0;
+   
+   @Override
+   public Set<Instance> processRequest(Instance instance) {
+      //TODO
+   }
+   
+   @Override
+	public void report(Reporter reporter) {
+		reporter.report(getClass().getSimpleName(), "invocations = " + invocations);
+   }
+   
+private static class ConstantPropagationVisitor extends Visitor {
+   public ConstantPropagationVisitor() {
+    //TODO  
+   }
+   public Expression getExpression() {
+    //TODO
+    return new Expression();
+   }
+   @Override
+   public void postVisit(Constant constant) {
+   //TODO
+   }
+   @Override
+   public void postVisit(Variable variable) {
+   //TODO
+   }
+   @Override
+	public void postVisit(Operation operation) throws VisitorException {
+
+   }
+}
+}
