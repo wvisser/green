2b681cddb07c979c342f8003b98f2b66446fda89
Simon Dennis <simondennis9@gmail.com>
2018-08-03 17:34:39 +0200

    fixed some things

--- src/za/ac/sun/cs/green/service/simplify/ConstantPropogation.java
+++ src/za/ac/sun/cs/green/service/simplify/ConstantPropogation.java
@@ -17,6 +17,7 @@ import za.ac.sun.cs.green.expr.Variable;
 import za.ac.sun.cs.green.expr.Visitor;
 import za.ac.sun.cs.green.expr.VisitorException;
 import za.ac.sun.cs.green.service.BasicService;
+import za.ac.sun.cs.green.util.Reporter;
 
 /**
  *
@@ -24,93 +25,101 @@ import za.ac.sun.cs.green.service.BasicService;
  */
 public class ConstantPropogation extends BasicService {
 
-    public ConstantPropogation(Green solver) {
-        super(solver);
-    }
+	private int invocations = 0;
+	
+	public ConstantPropogation(Green solver) {
+		super(solver);
+	}
 
-    @Override
-    public Set<Instance> processRequest(Instance instance) {
-        @SuppressWarnings("unchecked")
-        Set<Instance> result = (Set<Instance>) instance.getData(getClass());
-        if (result == null) {
-            final Map<Variable, Variable> map = new HashMap<Variable, Variable>();
-            final Expression e = constant_propogation(instance.getFullExpression(), map);
-            final Instance i = new Instance(getSolver(), instance.getSource(), null, e);
-            result = Collections.singleton(i);
-            instance.setData(getClass(), result);
-        }
-        return result;
-    }
+	@Override
+	public Set<Instance> processRequest(Instance instance) {
+		@SuppressWarnings("unchecked")
+		Set<Instance> result = (Set<Instance>) instance.getData(getClass());
+		if (result == null) {
+			final Map<Variable, Variable> map = new HashMap<Variable, Variable>();
+			final Expression e = constant_propogation(instance.getFullExpression(), map);
+			final Instance i = new Instance(getSolver(), instance.getSource(), null, e);
+			result = Collections.singleton(i);
+			instance.setData(getClass(), result);
+		}
+		return result;
+	}
+	
+	@Override
+	public void report(Reporter reporter) {
+		reporter.report(getClass().getSimpleName(), "invocations = " + invocations);
+	}
 
-    public Expression constant_propogation(Expression expression, Map<Variable, Variable> map) {
-        try {
-            log.log(Level.FINEST, "Before Constant Propogation: " + expression);
-            ConstantPropogationVisitor constantPropogationVisitor = new ConstantPropogationVisitor();
-            expression.accept(constantPropogationVisitor);
-            Expression processed = constantPropogationVisitor.getExpression();
-            log.log(Level.FINEST, "After Constant Propogation: " + processed);
-            return processed;
-        } catch (VisitorException x) {
-            log.log(Level.SEVERE,
-                    "encountered an exception -- this should not be happening!",
-                    x);
-        }
-        return null;
-    }
+	public Expression constant_propogation(Expression expression, Map<Variable, Variable> map) {
+		try {
+			log.log(Level.FINEST, "Before Constant Propogation: " + expression);
+			invocations++;
+			ConstantPropogationVisitor constantPropogationVisitor = new ConstantPropogationVisitor();
+			expression.accept(constantPropogationVisitor);
+			expression = constantPropogationVisitor.getExpression();
+			log.log(Level.FINEST, "After Constant Propogation: " + expression);
+			return expression;
+		} catch (VisitorException x) {
+			log.log(Level.SEVERE,
+					"encountered an exception -- this should not be happening!",
+					x);
+		}
+		return null;
+	}
 
-    private static class ConstantPropogationVisitor extends Visitor {
+	private static class ConstantPropogationVisitor extends Visitor {
 
-        private Stack<Expression> stack;
-        private HashMap<IntVariable, IntConstant> variables;
+		private Stack<Expression> stack;
+		private HashMap<IntVariable, IntConstant> variables;
 
-        public ConstantPropogationVisitor() {
-            this.stack = new Stack<Expression>();
-            this.variables = new HashMap<IntVariable, IntConstant>();
-        }
-        
-        public Expression getExpression() {
-            return stack.pop();
-        }
+		public ConstantPropogationVisitor() {
+			this.stack = new Stack<Expression>();
+			this.variables = new HashMap<IntVariable, IntConstant>();
+		}
 
-        @Override
-        public void postVisit(Constant constant) {
-            stack.push(constant);
-        }
-        
-        @Override
-        public void postVisit(Variable variable) {
-            stack.push(variable);
-        }
+		public Expression getExpression() {
+			return stack.pop();
+		}
 
-        @Override
-        public void postVisit(IntVariable variable) {
-            if (variables.containsKey(variable)) {
-                System.out.println("replacing variable " + variable.getName() + " with value " + variables.get(variable));
-                stack.push(variables.get(variable));
-            } else {
-                System.out.println("not replacing variable " + variable.getName());
-                stack.push(variable);
-            }
-        }
+		@Override
+		public void postVisit(Constant constant) {
+			stack.push(constant);
+		}
 
-        @Override
-        public void postVisit(Operation operation) {
-            Operation.Operator op = operation.getOperator();
-            if (op == Operation.Operator.EQ) {
-                Expression r = stack.pop();
-                Expression l = stack.pop();
-                if (r instanceof IntConstant &&
-                    l instanceof IntVariable) {
-                    System.out.println("adding variable " + l + " to list with value " + r);
-                    variables.put((IntVariable) l, (IntConstant) r);
-                }
-                stack.push(l);
-                stack.push(r);
-            } else {
-                stack.push(operation);
-            }
-        }
+		@Override
+		public void postVisit(Variable variable) {
+			stack.push(variable);
+		}
 
-    }
+		@Override
+		public void postVisit(IntVariable variable) {
+			if (variables.containsKey(variable)) {
+				System.out.println("replacing variable " + variable.getName() + " with value " + variables.get(variable));
+				stack.push(variables.get(variable));
+			} else {
+				System.out.println("not replacing variable " + variable.getName());
+				stack.push(variable);
+			}
+		}
+
+		@Override
+		public void postVisit(Operation operation) {
+			Operation.Operator op = operation.getOperator();
+			if (op == Operation.Operator.EQ) {
+				Expression r = stack.pop();
+				Expression l = stack.pop();
+				if (r instanceof IntConstant
+						&& l instanceof IntVariable) {
+					System.out.println("adding variable " + l + " to list with value " + r);
+					variables.put((IntVariable) l, (IntConstant) r);
+				}
+				stack.push(l);
+				stack.push(r);
+			} else {
+				stack.push(operation);
+			}
+		}
+
+	}
 
 }
