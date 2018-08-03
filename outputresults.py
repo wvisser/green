19b544cc035aeccbc64a920920c05970f57577b0
Craig Armstrong <19165072@sun.ac.za>
2018-08-03 20:40:55 +0200

    Changed outputresults.py

--- outputresults.py
+++ outputresults.py
@@ -3,20 +3,15 @@ import sys
 
 tree = ET.parse('./bin/junit/TEST-za.ac.sun.cs.green.EntireSuite.xml')
 root = tree.getroot()
-hadfail = False;
+hadfail = False
 
 for test in root.findall("./testcase"):
     if test.findall("failure"):
         hadfail = True
-        if test.attrib['classname'] == "za.ac.sun.cs.green.service.canonizer.SATCanonizerTest":
-            print "SATCanonizerTest: ", test.attrib['name'], " Failed", test.find("failure").attrib['message']
-        else:
-            print "Z3Test: ", test.attrib['name'], " Failed", test.find("failure").attrib['message']
+        print "Failed ", test
+        # print "Test", test.attrib['name'], " Failed", test.find("failure").attrib['message']
     else:
-        if test.attrib['classname'] == "za.ac.sun.cs.green.service.canonizer.SATCanonizerTest":
-            print "SATCanonizerTest: ", test.attrib['name'], "Passed"
-        else:
-            print "Z3Test: ", test.attrib['name'], "Passed"
+        print "Passed ", test
 
 if hadfail == True:
     sys.exit(1)
