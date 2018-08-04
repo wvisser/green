4860f28986a1d0ffff8b7d83b51743ec0c31aad7
Craig Armstrong <19165072@sun.ac.za>
2018-08-04 17:18:51 +0200

    Fixed Propogation typos

--- test/za/ac/sun/cs/green/service/simplify/SimplificationConstantPropagationTest.java
+++ test/za/ac/sun/cs/green/service/simplify/SimplificationConstantPropagationTest.java
@@ -18,7 +18,7 @@ import za.ac.sun.cs.green.expr.IntVariable;
 import za.ac.sun.cs.green.expr.Operation;
 import za.ac.sun.cs.green.util.Configuration;
 
-public class SimplificationConstantPropogationTest {
+public class SimplificationConstantPropagationTest {
 
 	public static Green solver;
 
@@ -29,7 +29,7 @@ public class SimplificationConstantPropogationTest {
 			props.setProperty("green.services", "sat");
 			props.setProperty("green.service.sat", "(simplify sink)");
 			//props.setProperty("green.service.sat", "(canonize sink)");
-			props.setProperty("green.service.sat.simplify", "za.ac.sun.cs.green.service.simplify.ConstantPropogation");
+			props.setProperty("green.service.sat.simplify", "za.ac.sun.cs.green.service.simplify.ConstantPropagation");
 			//props.setProperty("green.service.sat.canonize", "za.ac.sun.cs.green.service.canonizer.SATCanonizerService");
 			props.setProperty("green.service.sat.sink", "za.ac.sun.cs.green.service.sink.SinkService");
 			Configuration config = new Configuration(solver, props);
