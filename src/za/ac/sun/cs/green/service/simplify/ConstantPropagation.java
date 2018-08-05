10b8fd9a45c1cc88258cc43feb1ecb69361c56cd
Simoné Van Zyl <18395317@sun.ac.za>
2018-08-05 13:28:28 +0200

    Updates

--- src/za/ac/sun/cs/green/service/simplify/ConstantPropagation.java
+++ src/za/ac/sun/cs/green/service/simplify/ConstantPropagation.java
@@ -5,6 +5,12 @@ package za.ac.sun.cs.green.service.simplify;
    3. do full propagation until you reach a fixpoint
 
 */
+/* External */
+import java.util.Stack;
+
+/* local */
+import za.ac.sun.cs.green.Instance;
+import za.ac.sun.cs.green.Green;
 import za.ac.sun.cs.green.expr.Expression;
 import za.ac.sun.cs.green.service.BasicService;
 import za.ac.sun.cs.green.expr.Constant;
@@ -19,20 +25,24 @@ import za.ac.sun.cs.green.util.Reporter;
 
 public class ConstantPropagation extends BasicService {
    private int invocations = 0;
-   
+
+   public ConstantPropagation(Green solver) {
+       super(solver);
+   }
+
    @Override
    public Set<Instance> processRequest(Instance instance) {
       //TODO
    }
-   
+
    @Override
 	public void report(Reporter reporter) {
 		reporter.report(getClass().getSimpleName(), "invocations = " + invocations);
    }
-   
+
 private static class ConstantPropagationVisitor extends Visitor {
    public ConstantPropagationVisitor() {
-    //TODO  
+    //TODO
    }
    public Expression getExpression() {
     //TODO
