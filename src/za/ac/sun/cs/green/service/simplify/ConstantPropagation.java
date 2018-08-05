bc7d340ad303b1c0e5df33072d91ddaaa28c0457
Craig James Armstrong <19165072@sun.ac.za>
2018-08-05 20:26:44 +0200

    Corrected print statements

--- src/za/ac/sun/cs/green/service/simplify/ConstantPropagation.java
+++ src/za/ac/sun/cs/green/service/simplify/ConstantPropagation.java
@@ -177,7 +177,7 @@ public class ConstantPropagation extends BasicService {
 					l = variables.get(l);
 				}
 
-				System.out.println("Pushing to stack (area2) " + l + nop + r);
+				System.out.println("Pushing to stack (area1) " + l + nop + r);
 				stack.push(new Operation(nop, l, r));
                 
 				// if ((r instanceof IntVariable) && (l instanceof IntVariable) && (((IntVariable) r).getName().compareTo(((IntVariable) l).getName()) < 0)) {
@@ -204,7 +204,7 @@ public class ConstantPropagation extends BasicService {
 					l = variables.get(l);
 				}
 
-				System.out.println("Pushing to stack (area3) " + l + nop + r);
+				System.out.println("Pushing to stack (area2) " + l + nop + r);
 				stack.push(new Operation(nop, l, r));
                 
                 // Operation newOp = new Operation(op, l, r);
