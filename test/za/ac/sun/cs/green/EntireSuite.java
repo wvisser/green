5bd756cc8f3b10719b0d9faf7a58a03037879d5c
Ricardo Romeo Luiz <20165714@sun.ac.za>
2018-08-09 23:03:29 +0200

    Uncomment of suit class addition.

--- test/za/ac/sun/cs/green/EntireSuite.java
+++ test/za/ac/sun/cs/green/EntireSuite.java
@@ -40,9 +40,9 @@ import za.ac.sun.cs.green.util.SetTaskManagerTest;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
 	SATCanonizerTest.class,
-	SATZ3Test.class,
+	SATZ3Test.class/*,
 
-    OnlyConstantPropagationTest.class/*,
+    OnlyConstantPropagationTest.class,
     SimplificationConstantPropagationTest.class*/
 })
 
