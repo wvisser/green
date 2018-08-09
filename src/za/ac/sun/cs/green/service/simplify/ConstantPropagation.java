066630b81cfbd3033cd19797439214b0dd59a78d
Sedick David Baker Effendi <20056354@sun.ac.za>
2018-08-09 13:52:46 +0200

    The start of a long day

--- src/za/ac/sun/cs/green/service/simplify/ConstantPropagation.java
+++ src/za/ac/sun/cs/green/service/simplify/ConstantPropagation.java
@@ -1,18 +1,13 @@
 package za.ac.sun.cs.green.service.simplify;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 import java.util.SortedMap;
 import java.util.SortedSet;
 import java.util.Stack;
 import java.util.TreeMap;
 import java.util.TreeSet;
-import java.util.logging.Level;
 
 import za.ac.sun.cs.green.Green;
-import za.ac.sun.cs.green.Instance;
 import za.ac.sun.cs.green.expr.Constant;
 import za.ac.sun.cs.green.expr.Expression;
 import za.ac.sun.cs.green.expr.IntConstant;
@@ -22,7 +17,6 @@ import za.ac.sun.cs.green.expr.Variable;
 import za.ac.sun.cs.green.expr.Visitor;
 import za.ac.sun.cs.green.expr.VisitorException;
 import za.ac.sun.cs.green.service.BasicService;
-import za.ac.sun.cs.green.util.Reporter;
 
 public class ConstantPropagation extends BasicService {
 	
@@ -34,50 +28,7 @@ public class ConstantPropagation extends BasicService {
 	public ConstantPropagation(Green solver) {
 		super(solver);
 	}
-	
-	@Override
-	public Set<Instance> processRequest(Instance instance) {
-		@SuppressWarnings("unchecked")
-		Set<Instance> result = (Set<Instance>) instance.getData(getClass());
-		if (result == null) {
-			final Map<Variable, Variable> map = new HashMap<Variable, Variable>();
-			final Expression e = propagate(instance.getFullExpression(), map);
-			final Instance i = new Instance(getSolver(), instance.getSource(), null, e);
-			result = Collections.singleton(i);
-			instance.setData(getClass(), result);
-		}
-		return result;
-	}
 
-	@Override
-	public void report(Reporter reporter) {
-		reporter.report(getClass().getSimpleName(), "invocations = " + invocations);
-	}
-	
-	public Expression propagate(Expression expression,
-			Map<Variable, Variable> map) {
-		try {
-			log.log(Level.FINEST, "Before Canonization: " + expression);
-			invocations++;
-			OrderingVisitor orderingVisitor = new OrderingVisitor();
-			expression.accept(orderingVisitor);
-			expression = orderingVisitor.getExpression();
-			PropagationVisitor canonizationVisitor = new PropagationVisitor();
-			expression.accept(canonizationVisitor);
-			Expression canonized = canonizationVisitor.getExpression();
-			if (canonized != null) {
-				canonized = new Renamer(map,
-						canonizationVisitor.getVariableSet()).rename(canonized);
-			}
-			log.log(Level.FINEST, "After Canonization: " + canonized);
-			return canonized;
-		} catch (VisitorException x) {
-			log.log(Level.SEVERE,
-					"encountered an exception -- this should not be happening!",
-					x);
-		}
-		return null;
-	}
 	
 	private static class OrderingVisitor extends Visitor {
 
@@ -630,51 +581,6 @@ public class ConstantPropagation extends BasicService {
 				Expression r = scale(factor, o.getOperand(1));
 				return new Operation(p, l, r);
 			}
-		}
-
-	}
-
-	private static class Renamer extends Visitor {
-
-		private Map<Variable, Variable> map;
-
-		private Stack<Expression> stack;
-
-		public Renamer(Map<Variable, Variable> map,
-				SortedSet<IntVariable> variableSet) {
-			this.map = map;
-			stack = new Stack<Expression>();
-		}
-
-		public Expression rename(Expression expression) throws VisitorException {
-			expression.accept(this);
-			return stack.pop();
-		}
-
-		@Override
-		public void postVisit(IntVariable variable) {
-			Variable v = map.get(variable);
-			if (v == null) {
-				v = new IntVariable("v" + map.size(), variable.getLowerBound(),
-						variable.getUpperBound());
-				map.put(variable, v);
-			}
-			stack.push(v);
-		}
-
-		@Override
-		public void postVisit(IntConstant constant) {
-			stack.push(constant);
-		}
-
-		@Override
-		public void postVisit(Operation operation) {
-			int arity = operation.getOperator().getArity();
-			Expression operands[] = new Expression[arity];
-			for (int i = arity; i > 0; i--) {
-				operands[i - 1] = stack.pop();
-			}
-			stack.push(new Operation(operation.getOperator(), operands));
 		}
 
 	}
