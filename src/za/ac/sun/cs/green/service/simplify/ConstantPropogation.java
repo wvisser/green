0a9b20ef44f495aa8cdae011098bd882c1a3d5ed
Justin Stigling <stigling75@gmail.com>
2018-08-10 21:40:27 +0200

    Added a visitor

--- src/za/ac/sun/cs/green/service/simplify/ConstantPropogation.java
+++ src/za/ac/sun/cs/green/service/simplify/ConstantPropogation.java
@@ -11,6 +11,8 @@ import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.logging.Level;
 
+import com.sun.javafx.binding.IntegerConstant;
+
 import za.ac.sun.cs.green.Instance;
 import za.ac.sun.cs.green.Green;
 import za.ac.sun.cs.green.expr.Expression;
@@ -52,8 +54,76 @@ public class ConstantPropogation extends BasicService {
     }
 
 	public Expression simplify(Expression expression, Map<Variable, Variable> map) {
+		log.log(Level.FINEST, "Before simplifying: " + expression);
+		invocations += 1;
+		
 		return expression;
 	}
 
+	public class SimplifyVisitor extends Visitor {
+		private Stack<Expression> stack;
+		private Map<IntVariable, IntConstant> map;
+
+		public SimplifyVisitor() {
+			stack = new Stack<Expression>();
+			map = new TreeMap<IntVariable, IntConstant>();
+		}
+
+		public Expression getExpression() {
+			stack.pop();
+		}
+
+		@Override
+		public void postVisit(IntConstant constant) {
+			stack.push(constant);
+		}
+
+		@Override
+		public void postVisit(Operation operation) {
+			Operation.Operator op = operation.getOperator();
+			if (op.equals(Operation.Operator.EQ)) {
+				Expression opL = operation.getOperand(0);
+				Expression opR = operation.getOperand(1);
+				if ((opL instanceof IntConstant) && (opR instanceof IntVariable)) {
+					map.put((IntVariable) opR, (IntConstant) opL);
+				} else if ((opL instanceof IntVariable) && (opR instanceof IntConstant)) {
+					map.put((IntVarible) opL, (IntConstant) opR);
+				}
+
+			}
+		}
+
+		@Override
+		public void postVisit(IntVariable variable) {
+			stack.push(variable);
+		}
+
+		@Override
+		public void preVisit(Operation operation) {
+			Operation.Operator op = operation.getOperator();
+
+			if (stack.size >= 2) {
+				Expression right = stack.pop();
+				Expression left = stack.pop();
+				if (!op.equals(Operation.Operator.EQ)) {
+					if (left instanceof IntVariable) {
+						if (map.containsKey(left)) {
+							left = map.get(left);
+						}
+					}
+					if (right instanceof IntVariable) {
+						if (map.containsKey(right)) {
+							right = map.get(right);
+						}
+					}
+				}
+
+				Operation e = new Operation(Operation.Operator.EQ, left, right);
+				stack.push(e);
+			}
+
+		}
+
+	}
 
 }
