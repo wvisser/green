afb86611c86a37b40cfca70f2e0eab4172229c05
Jason Jackson <20131836@sun.ac.za>
2018-08-10 11:46:16 +0000

    started work on new idea

--- src/za/ac/sun/cs/green/service/simplify/ConstantPropagation.java
+++ src/za/ac/sun/cs/green/service/simplify/ConstantPropagation.java
@@ -13,25 +13,79 @@ import za.ac.sun.cs.green.expr.Variable;
 import za.ac.sun.cs.green.expr.Visitor;
 import za.ac.sun.cs.green.expr.VisitorException;
 
-public class ConstantPropagation extends Visitor {
-	
-	public static void main(String args[]) {
-		IntVariable x = new IntVariable("x", 0, 99);
-		IntVariable y = new IntVariable("y", 0, 99);
-		IntVariable z = new IntVariable("z", 0, 99);
-		
-		IntConstant c = new IntConstant(1);
-		IntConstant c10 = new IntConstant(10);
-		IntConstant c3 = new IntConstant(3);
-		
-		Operation o1 = new Operation(Operation.Operator.EQ, x, c); // o1 : x = 1
-		Operation o2 = new Operation(Operation.Operator.ADD, x, y); // o2 : (x + y)
-		Operation o3 = new Operation(Operation.Operator.EQ, o2, c10); // o3 : x+y = 10
-		Operation o4 = new Operation(Operation.Operator.AND, o1, o3); // o4 : x = 1 && (x+y) = 10
-		for (Expression o: o4.getOperands()) {
-			
-			System.out.println(((Operation) o));
-		}
-	}
-	
+private static class ConstantPropagation extends Visitor {
+
+    private Stack<Expression> stack;
+
+    public OrderingVisitor() {
+        stack = new Stack<Expression>();
+    }
+
+    public Expression getExpression() {
+        return stack.pop();
+    }
+
+    @Override
+    public void postVisit(IntConstant constant) {
+        stack.push(constant);
+    }
+
+    @Override
+    public void postVisit(IntVariable variable) {
+        stack.push(variable);
+    }
+
+    @Override
+    public void postVisit(Operation operation) throws VisitorException {
+        Operation.Operator op = operation.getOperator();
+        Operation.Operator nop = null;
+        switch (op) {
+            case EQ:
+                nop = Operation.Operator.EQ;
+                break;
+            case NE:
+                nop = Operation.Operator.NE;
+                break;
+            case LT:
+                nop = Operation.Operator.GT;
+                break;
+            case LE:
+                nop = Operation.Operator.GE;
+                break;
+            case GT:
+                nop = Operation.Operator.LT;
+                break;
+            case GE:
+                nop = Operation.Operator.LE;
+                break;
+            default:
+                break;
+        }
+        if (nop != null) {
+            Expression r = stack.pop();
+            Expression l = stack.pop();
+            if ((r instanceof IntVariable)
+                    && (l instanceof IntVariable)
+                    && (((IntVariable) r).getName().compareTo(
+                            ((IntVariable) l).getName()) < 0)) {
+                stack.push(new Operation(nop, r, l));
+            } else if ((r instanceof IntVariable)
+                    && (l instanceof IntConstant)) {
+                stack.push(new Operation(nop, r, l));
+            } else {
+                stack.push(operation);
+            }
+        } else if (op.getArity() == 2) {
+            Expression r = stack.pop();
+            Expression l = stack.pop();
+            stack.push(new Operation(op, l, r));
+        } else {
+            for (int i = op.getArity(); i > 0; i--) {
+                stack.pop();
+            }
+            stack.push(operation);
+        }
+    }
+
 }
+
