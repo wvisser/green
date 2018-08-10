7ea8c1819f4e473cab18676d1a743beae805b966
Ricardo Romeo Luiz <20165714@sun.ac.za>
2018-08-10 22:56:56 +0200

    Successful constant propagation for simple test case.

--- src/za/ac/sun/cs/green/service/simplify/ConstantPropagation.java
+++ src/za/ac/sun/cs/green/service/simplify/ConstantPropagation.java
@@ -56,100 +56,18 @@ public class ConstantPropagation extends BasicService {
         try {
             log.log(Level.FINEST, "Before Constant Propagation: " + expression);
             invocations++;
-            OrderingVisitor orderingVisitor = new OrderingVisitor();
-            expression.accept(orderingVisitor);
-            expression = orderingVisitor.getExpression();
 
             ConstantPropagationVisitor constantPropagationVisitor = new ConstantPropagationVisitor();
             expression.accept(constantPropagationVisitor);
             Expression propagated = constantPropagationVisitor.getExpression();
 
-            if (propagated != null) {
-                System.out.println("Prob?");
-            }
-
-            log.log(Level.FINEST, "After Constant Propagation: " + expression);
-            //return expression;
+            log.log(Level.FINEST, "After Constant Propagation: " + propagated);
             return propagated;
         } catch (VisitorException x) {
             log.log(Level.SEVERE, "encountered an exception -- this should not be happening!", x);
         }
+
         return null;
-    }
-
-    private static class OrderingVisitor extends Visitor {
-
-        private Stack<Expression> stack;
-
-        public OrderingVisitor() {
-            stack = new Stack<Expression>();
-        }
-
-        public Expression getExpression() {
-            return stack.pop();
-        }
-
-        @Override
-        public void postVisit(IntConstant constant) {
-            stack.push(constant);
-        }
-
-        @Override
-        public void postVisit(IntVariable variable) {
-            stack.push(variable);
-        }
-
-        @Override
-        public void postVisit(Operation operation) throws VisitorException {
-            Operation.Operator op = operation.getOperator();
-            Operation.Operator nop = null;
-            switch (op) {
-                case EQ:
-                    nop = Operation.Operator.EQ;
-                    break;
-                case NE:
-                    nop = Operation.Operator.NE;
-                    break;
-                case LT:
-                    nop = Operation.Operator.GT;
-                    break;
-                case LE:
-                    nop = Operation.Operator.GE;
-                    break;
-                case GT:
-                    nop = Operation.Operator.LT;
-                    break;
-                case GE:
-                    nop = Operation.Operator.LE;
-                    break;
-                default:
-                    break;
-            }
-            if (nop != null) {
-                Expression r = stack.pop();
-                Expression l = stack.pop();
-                if ((r instanceof IntVariable)
-                        && (l instanceof IntVariable)
-                        && (((IntVariable) r).getName().compareTo(
-                        ((IntVariable) l).getName()) < 0)) {
-                    stack.push(new Operation(nop, r, l));
-                } else if ((r instanceof IntVariable)
-                        && (l instanceof IntConstant)) {
-                    stack.push(new Operation(nop, r, l));
-                } else {
-                    stack.push(operation);
-                }
-            } else if (op.getArity() == 2) {
-                Expression r = stack.pop();
-                Expression l = stack.pop();
-                stack.push(new Operation(op, l, r));
-            } else {
-                for (int i = op.getArity(); i > 0; i--) {
-                    stack.pop();
-                }
-                stack.push(operation);
-            }
-        }
     }
 
     private static class ConstantPropagationVisitor extends Visitor {
@@ -160,7 +78,7 @@ public class ConstantPropagation extends BasicService {
             stack = new Stack<Expression>();
 
             /**
-             * Do not what to write a hash function.
+             * TreeMap because I not what to write a hash function.
              */
             variableMap = new TreeMap<IntVariable, IntConstant>();
         }
@@ -186,8 +104,7 @@ public class ConstantPropagation extends BasicService {
         @Override
         public void postVisit(Variable variable) {
             if (variable instanceof IntVariable) {
-                //variableMap.put((IntVariable) variable, null);
-                stack.push(new Operation(Operation.Operator.MUL, Operation.ONE, variable));
+                stack.push(variable);
             } else {
                 stack.clear();
             }
@@ -205,6 +122,7 @@ public class ConstantPropagation extends BasicService {
                     } else if ((r instanceof IntVariable) && (l instanceof IntConstant)) {
                         variableMap.put((IntVariable) r, (IntConstant) l);
                     }
+                    stack.push(new Operation(op, l, r));
                 } else if (!op.equals(Operation.Operator.EQ)) {
                     if (variableMap.containsKey(l)) {
                         l = variableMap.get(l);
