51778630f101b3dcdc19012dc57b83a38451d857
Emer Mae Butler <emermae@gmail.com>
2018-08-11 23:20:40 +0200

    Finished the ScoutVisitor (and fixed the damned NullLogger)

--- test/za/ac/sun/cs/green/service/simplify/OnlyConstantPropogationTest.java
+++ test/za/ac/sun/cs/green/service/simplify/OnlyConstantPropogationTest.java
@@ -30,7 +30,7 @@ public class OnlyConstantPropogationTest {
 			props.setProperty("green.service.sat", "(simplify sink)");
 			//props.setProperty("green.service.sat", "(canonize sink)");
 			props.setProperty("green.service.sat.simplify",
-					"za.ac.sun.cs.green.service.simplify.ConstantPropogation");
+					"za.ac.sun.cs.green.service.simplify.ConstantPropagation");
 			//props.setProperty("green.service.sat.canonize",
 			//		"za.ac.sun.cs.green.service.canonizer.SATCanonizerService");
 			
@@ -67,8 +67,25 @@ public class OnlyConstantPropogationTest {
 		Operation o1 = new Operation(Operation.Operator.EQ, x, c); // o1 : x = 1
 		Operation o2 = new Operation(Operation.Operator.ADD, x, y); // o2 : (x + y)
 		Operation o3 = new Operation(Operation.Operator.EQ, o2, c10); // o3 : x+y = 10
-		Operation o4 = new Operation(Operation.Operator.AND, o1, o3); // o4 : x = 1 && (x+y) = 10 
+		Operation o4 = new Operation(Operation.Operator.AND, o1, o3); // o4 : x = 1 && (x+y) = 10
 		check(o4, "(x==1)&&((1+y)==10)");
+	}
+
+
+	@Test
+	public void test01() {
+		IntVariable x = new IntVariable("x", 0, 99);
+		IntVariable y = new IntVariable("y", 0, 99);
+		IntVariable z = new IntVariable("z", 0, 99);
+		IntConstant c = new IntConstant(1);
+		IntConstant c10 = new IntConstant(10);
+		IntConstant c3 = new IntConstant(3);
+		Operation o1 = new Operation(Operation.Operator.EQ, x, c); // o1 : x = 1
+		Operation o2 = new Operation(Operation.Operator.EQ, c10, z); // o2 : 10 = z
+		Operation o4 = new Operation(Operation.Operator.AND, o1, o2); // o4 : x = 1 && 9 = z
+
+
+		check(o4, "(x==1)&&(10==z)");
 	}
 
 }
