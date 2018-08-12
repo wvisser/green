feff580ca74df698e741c5805246894a73cc0889
RPasch <pasch.k.r@gmail.com>
2018-08-12 12:57:35 +0200

    Update EntireSuite.java

--- test/za/ac/sun/cs/green/EntireSuite.java
+++ test/za/ac/sun/cs/green/EntireSuite.java
@@ -32,11 +32,13 @@ import za.ac.sun.cs.green.service.z3.SATZ3Test;
 import za.ac.sun.cs.green.util.ParallelSATTest;
 import za.ac.sun.cs.green.util.SetServiceTest;
 import za.ac.sun.cs.green.util.SetTaskManagerTest;
+import za.ac.sun.cs.green.service.simplify.OnlyConstantPropogationTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
 	SATCanonizerTest.class,
 	SATZ3Test.class
+	OnlyConstantPropogationTest.class
 })
 
 public class EntireSuite {
