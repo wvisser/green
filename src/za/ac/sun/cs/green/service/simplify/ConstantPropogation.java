6263164e76af30e34b9f5767d8dcc24ee50dface
Jonathan Angus Hulley <19894295@sun.ac.za>
2018-08-12 19:39:52 +0200

    Added Propagation Part of the tutorial

---
+++ src/za/ac/sun/cs/green/service/simplify/ConstantPropogation.java
@@ -0,0 +1,157 @@
+package za.ac.sun.cs.green.service.simplify;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.logging.Level;
+
+import za.ac.sun.cs.green.Instance;
+import za.ac.sun.cs.green.Green;
+import za.ac.sun.cs.green.expr.Expression;
+import za.ac.sun.cs.green.service.BasicService;
+import za.ac.sun.cs.green.util.Reporter;
+import za.ac.sun.cs.green.expr.Constant;
+import za.ac.sun.cs.green.expr.IntConstant;
+import za.ac.sun.cs.green.expr.IntVariable;
+import za.ac.sun.cs.green.expr.Operation;
+import za.ac.sun.cs.green.expr.Variable;
+import za.ac.sun.cs.green.expr.Visitor;
+import za.ac.sun.cs.green.expr.VisitorException;
+
+public class ConstantPropogation extends BasicService {
+
+	/**
+	 * Number of times the slicer has been invoked.
+	 */
+	private int invocations = 0;
+
+	public ConstantPropogation(Green solver) {
+		super(solver);
+	}
+
+	@Override
+	public Set<Instance> processRequest(Instance instance) {
+		@SuppressWarnings("unchecked")
+		Set<Instance> result = (Set<Instance>) instance.getData(getClass());
+		if (result == null) {
+			final Map<Variable, Variable> map = new HashMap<Variable, Variable>();
+			final Expression e = canonize(instance.getFullExpression(), map);
+			final Instance i = new Instance(getSolver(), instance.getSource(), null, e);
+			result = Collections.singleton(i);
+			instance.setData(getClass(), result);
+		}
+		return result;
+	}
+
+	@Override
+	public void report(Reporter reporter) {
+		reporter.report(getClass().getSimpleName(), "invocations = " + invocations);
+	}
+
+	public Expression canonize(Expression expression, Map<Variable, Variable> map) {
+		try {
+			log.log(Level.FINEST, "Before Propagation: " + expression);
+			invocations++;
+			OrderingVisitor orderingVisitor = new OrderingVisitor();
+			expression.accept(orderingVisitor);
+			expression = orderingVisitor.getExpression();
+			log.log(Level.FINEST, "After Propagation: " + expression);
+			return expression;
+
+		} catch (VisitorException x) {
+			log.log(Level.SEVERE,
+					"encountered an exception -- this should not be happening!",
+					x);
+		}
+		return null;
+	}
+
+	private static class OrderingVisitor extends Visitor {
+
+		private Stack<Expression> stack;
+		private HashMap<String, Integer> varMap = new HashMap<String, Integer>();
+
+		public OrderingVisitor() {
+			stack = new Stack<Expression>();
+		}
+
+		public Expression getExpression() {
+			return stack.pop();
+		}
+
+		@Override
+		public void postVisit(IntConstant constant) {
+			stack.push(constant);
+		}
+
+		@Override
+		public void postVisit(IntVariable variable) {
+			stack.push(variable);
+		}
+
+		@Override
+		public void postVisit(Operation operation) throws VisitorException {
+			Operation.Operator op = operation.getOperator();
+			Operation.Operator nop = null;
+			Operation e = null;
+			//find out which opernd op is
+			switch (op) {
+			case EQ:
+				nop = Operation.Operator.EQ;
+				break;
+			case NE:
+				nop = Operation.Operator.NE;
+				break;
+			case LT:
+				nop = Operation.Operator.GT;
+				break;
+			case LE:
+				nop = Operation.Operator.GE;
+				break;
+			case GT:
+				nop = Operation.Operator.LT;
+				break;
+			case GE:
+				nop = Operation.Operator.LE;
+				break;
+			default:
+				break;
+			}
+
+			if (op.equals(Operation.Operator.EQ)) {
+				Expression r = stack.pop();
+				Expression l = stack.pop();
+
+				if((r instanceof IntVariable) && (l instanceof IntConstant)){
+					varMap.put(r.toString(), Integer.parseInt(l.toString()));
+				} else if ((l instanceof IntVariable) && (r instanceof IntConstant)){
+					varMap.put(l.toString(), Integer.parseInt(r.toString()));
+				}
+				e = new Operation(op, l, r);
+			} else {
+				Expression r = stack.pop();
+				Expression l = stack.pop();
+				if(r instanceof IntVariable){
+					if(varMap.containsKey(r.toString())){
+						r = new IntConstant(varMap.get(r.toString()));
+					}
+				}
+				if(r instanceof IntVariable){
+					if(varMap.containsKey(l.toString())){
+						l = new IntConstant(varMap.get(l.toString()));
+					}
+				}
+					e = new Operation(op, l, r);
+			}
+			stack.push(e);
+
+		}
+
+	}
+}
