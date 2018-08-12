63f47909543921a91ab71a7e2cb5b2e4c9635e6c
Qhama <19206674@sun.ac.za>
2018-08-12 18:09:27 +0200

    Update

--- test/za/ac/sun/cs/green/EntireSuite.java
+++ test/za/ac/sun/cs/green/EntireSuite.java
@@ -32,11 +32,15 @@ import za.ac.sun.cs.green.service.z3.SATZ3Test;
 import za.ac.sun.cs.green.util.ParallelSATTest;
 import za.ac.sun.cs.green.util.SetServiceTest;
 import za.ac.sun.cs.green.util.SetTaskManagerTest;
+import za.ac.sun.cs.green.src.service.simplify.OnlyConstantPropogationTest;
+//import za.ac.sun.cs.green.src.service.simplify.SimplificationConstantPropogationTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
 	SATCanonizerTest.class,
-	SATZ3Test.class
+	SATZ3Test.class,
+	OnlyConstantPropogationTest.class
+	//SimplifiedConstantPropogationTest.class
 })
 
 public class EntireSuite {
