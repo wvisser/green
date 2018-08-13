008a985900e5723c0edde92b93eea27e1daf7407
Michael John Shepherd <19059019@sun.ac.za>
2018-08-13 21:21:53 +0200

    test build

--- src/za/ac/sun/cs/green/service/simplify/ConstantPropogation.java
+++ src/za/ac/sun/cs/green/service/simplify/ConstantPropogation.java
@@ -132,7 +132,7 @@ public class ConstantPropogation extends BasicService {
 					}
 				}
 				Operation e = new Operation(operation.getOperator(), left, right);
-                log.log(Level.FINEST, left op(0) right);
+                log.log(Level.FINEST, op(0));
 				stack.push(e);
 			}
 
