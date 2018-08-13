b203634b6c2cd7fcbdd21fd2bdc39ef315de2ac3
Riaan van Niekerk <16579089@sun.ac.za>
2018-08-13 13:00:47 +0200

    Adding new test

--- test/za/ac/sun/cs/green/EntireSuite.java
+++ test/za/ac/sun/cs/green/EntireSuite.java
@@ -39,8 +39,9 @@ import za.ac.sun.cs.green.util.SetTaskManagerTest;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
 	SATCanonizerTest.class,
-	SATZ3Test.class
-})
+	SATZ3Test.class,
+	OnlyConstantPropagationTest.class
+	})
 
 public class EntireSuite {
 
