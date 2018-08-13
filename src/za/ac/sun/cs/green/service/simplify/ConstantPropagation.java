b86ec8f7ae59c265d0edcead09365ae0e80621a3
Nuan <thenag47@gmail.com>
2018-08-13 16:00:47 +0200

    prop test 3

--- src/za/ac/sun/cs/green/service/simplify/ConstantPropagation.java
+++ src/za/ac/sun/cs/green/service/simplify/ConstantPropagation.java
@@ -1,14 +1,51 @@
-import za.ac.sun.cs.green.expr;
+package za.ac.sun.cs.green.service.simplify;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.logging.Level;
 
-public class ConstantPropagation {
+import za.ac.sun.cs.green.Instance;
+import za.ac.sun.cs.green.Green;
+import za.ac.sun.cs.green.expr.Expression;
+import za.ac.sun.cs.green.service.BasicService;
+import za.ac.sun.cs.green.util.Reporter;
+import za.ac.sun.cs.green.expr.Constant;
+import za.ac.sun.cs.green.expr.IntConstant;
+import za.ac.sun.cs.green.expr.IntVariable;
+import za.ac.sun.cs.green.expr.Operation;
+import za.ac.sun.cs.green.expr.Variable;
+import za.ac.sun.cs.green.expr.Visitor;
+import za.ac.sun.cs.green.expr.VisitorException;
 
-  static void propagate(Operator operator, Expression[] operands) {
-    if (operator.equals(Operation.Operator.AND)) {
-      for (Expression e : operands) {
-        System.out.println(e);
-      }
-    }
+public class ConstantPropagation extends BasicService {
+
+  public SATCanonizerService(Green solver) {
+    super(solver);
   }
 
+  @Override
+  public Set<Instance> processRequest(Instance instance) {
+    @SuppressWarnings("unchecked")
+    Set<Instance> result = (Set<Instance>) instance.getData(getClass());
+    if (result == null) {
+      final Map<Variable, Variable> map = new HashMap<Variable, Variable>();
+      final Expression e = propagate(instance.getFullExpression(), map);
+      final Instance i = new Instance(getSolver(), instance.getSource(), null, e);
+      result = Collections.singleton(i);
+      instance.setData(getClass(), result);
+    }
+    return result;
+  }
+
+  public Expression propagate(Expression expression,
+			Map<Variable, Variable> map) {
+
+  }
 }
