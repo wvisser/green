0acd6b4414e8d3721f79f9b96926af7a00843fec
Freddie Ryk De Villiers <20326033@sun.ac.za>
2018-08-13 18:48:29 +0200

    Add main tests

--- test/za/ac/sun/cs/green/EntireSuite.java
+++ test/za/ac/sun/cs/green/EntireSuite.java
@@ -26,6 +26,7 @@ import za.ac.sun.cs.green.service.factorizer.SATFactorizerTest;
 import za.ac.sun.cs.green.service.latte.CountLattETest;
 import za.ac.sun.cs.green.service.latte.CountLattEWithBounderTest;
 import za.ac.sun.cs.green.service.simplify.OnlyConstantPropagationTest;
+import za.ac.sun.cs.green.service.simplify.SimplificationConstantPropagationTest;
 import za.ac.sun.cs.green.service.slicer.ParallelSATSlicerTest;
 import za.ac.sun.cs.green.service.slicer.SATSlicerTest;
 import za.ac.sun.cs.green.service.z3.SATZ3JavaTest;
@@ -38,6 +39,7 @@ import za.ac.sun.cs.green.util.SetTaskManagerTest;
 @Suite.SuiteClasses({
 	SATCanonizerTest.class,
 	OnlyConstantPropagationTest.class,
+	SimplificationConstantPropagationTest.class,
 	SATZ3Test.class
 })
 
