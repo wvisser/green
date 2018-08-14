3d71dfa40e37288d987428f572d455899db272c7
Mr RA Venter <18439713@sun.ac.za>
2018-08-14 12:48:59 +0200

    Changed EntireSuite.java

--- test/za/ac/sun/cs/green/EntireSuite.java
+++ test/za/ac/sun/cs/green/EntireSuite.java
@@ -36,7 +36,8 @@ import za.ac.sun.cs.green.util.SetTaskManagerTest;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
 	SATCanonizerTest.class,
-	SATZ3Test.class
+	SATZ3Test.class,
+	OnlyConstantPropogationTest.class
 })
 
 public class EntireSuite {
