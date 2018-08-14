883f8657fc7fb81d199b7edfd21843526306b3b0
Dwaine Saunderson <19236980@sun.ac.za>
2018-08-14 02:31:04 +0200

    Moved Propagation Service to Seperate Source File for Testing

--- test/za/ac/sun/cs/green/service/simplify/OnlySimplificationServiceTest.java
+++ test/za/ac/sun/cs/green/service/simplify/OnlySimplificationServiceTest.java
@@ -30,7 +30,7 @@ public class OnlySimplificationServiceTest {
 			props.setProperty("green.service.sat", "(simplify sink)");
 			//props.setProperty("green.service.sat", "(canonize sink)");
 			props.setProperty("green.service.sat.simplify",
-					"za.ac.sun.cs.green.service.simplify.SimplificationService");
+					"za.ac.sun.cs.green.service.simplify.ConstantPropagationService");
 			//props.setProperty("green.service.sat.canonize",
 			//		"za.ac.sun.cs.green.service.canonizer.SATCanonizerService");
 			
