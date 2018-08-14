dde6aa56e929add9c6d3b742a29aef495b1c9da1
Nuan <thenag47@gmail.com>
2018-08-14 10:15:01 +0200

    fixed spacing problem

--- src/za/ac/sun/cs/green/service/simplify/ConstantPropagation.java
+++ src/za/ac/sun/cs/green/service/simplify/ConstantPropagation.java
@@ -26,63 +26,63 @@ import za.ac.sun.cs.green.expr.VisitorException;
 
 public class ConstantPropagation extends BasicService {
 
-  /**
-   * Number of times the slicer has been invoked.
-   */
-  private int invocations = 0;
+	/**
+	* Number of times the slicer has been invoked.
+	*/
+	private int invocations = 0;
 
-  public ConstantPropagation(Green solver) {
-    super(solver);
-  }
+	public ConstantPropagation(Green solver) {
+		super(solver);
+	}
 
-  @Override
-  public Set<Instance> processRequest(Instance instance) {
-    @SuppressWarnings("unchecked")
-    Set<Instance> result = (Set<Instance>) instance.getData(getClass());
-    if (result == null) {
-      final Map<Variable, Variable> map = new HashMap<Variable, Variable>();
-      final Expression e = propagate(instance.getFullExpression(), map);
-      final Instance i = new Instance(getSolver(), instance.getSource(), null, e);
-      result = Collections.singleton(i);
-      instance.setData(getClass(), result);
-    }
-    return result;
-  }
+	@Override
+	public Set<Instance> processRequest(Instance instance) {
+		@SuppressWarnings("unchecked")
+		Set<Instance> result = (Set<Instance>) instance.getData(getClass());
+		if (result == null) {
+			final Map<Variable, Variable> map = new HashMap<Variable, Variable>();
+			final Expression e = propagate(instance.getFullExpression(), map);
+			final Instance i = new Instance(getSolver(), instance.getSource(), null, e);
+			result = Collections.singleton(i);
+			instance.setData(getClass(), result);
+		}
+		return result;
+	}
 
-  @Override
+	@Override
 	public void report(Reporter reporter) {
 		reporter.report(getClass().getSimpleName(), "invocations = " + invocations);
 	}
 
-  public Expression propagate(Expression expression,
+	public Expression propagate(Expression expression,
 			Map<Variable, Variable> map) {
 
-    try {
-      log.log(Level.FINEST, "Before Canonization: " + expression);
-      invocations++;
-      PropagationVisitor propagationVisitor = new PropagationVisitor();
+				try {
+			log.log(Level.FINEST, "Before Canonization: " + expression);
+			invocations++;
+			PropagationVisitor propagationVisitor = new PropagationVisitor();
 			expression.accept(propagationVisitor);
 			Expression propagated = propagationVisitor.getExpression();
-      return propagated;
-    } catch (VisitorException x) {
-      log.log(Level.SEVERE,"encountered an exception -- this should not be happening!",x);
-    }
-    return null;
-  }
+			return propagated;
+		} catch (VisitorException x) {
+			log.log(Level.SEVERE,"encountered an exception -- this should not be happening!",x);
+		}
+		return null;
+	}
 
-  private static class PropagationVisitor extends Visitor {
+	private static class PropagationVisitor extends Visitor {
 
-    private Stack<Expression> stack;
+		private Stack<Expression> stack;
 
-    private IntVariable var;
+		private IntVariable var;
 
-    private IntConstant const1;
+		private IntConstant const1;
 
-    public PropagationVisitor() {
+		public PropagationVisitor() {
 			stack = new Stack<Expression>();
 		}
 
-    public Expression getExpression() {
+		public Expression getExpression() {
 			return stack.pop();
 		}
 
@@ -96,33 +96,33 @@ public class ConstantPropagation extends BasicService {
 			stack.push(variable);
 		}
 
-    @Override
+@Override
 		public void postVisit(Operation operation) throws VisitorException {
 			Operation.Operator op = operation.getOperator();
-      Expression r = stack.pop();
-      Expression l = stack.pop();
-      if (op == Operation.Operator.EQ) {
-        if (r instanceof IntVariable && l instanceof IntConstant) {
-          var = (IntVariable) r;
-          const1 = (IntConstant) l;
-        } else if (l instanceof IntVariable && r instanceof IntConstant) {
-          var = (IntVariable) l;
-          const1 = (IntConstant) r;
-        }
-        stack.push(new Operation(op, l, r));
-      } else {
-        if (r instanceof IntVariable) {
-          if (((IntVariable) r).getName().equals(var.getName())) {
-            r = const1;
-          }
-        }
-        if (l instanceof IntVariable) {
-          if (((IntVariable) l).getName().equals(var.getName())) {
-            l = const1;
-          }
-        }
-        stack.push(new Operation(op, l, r));
-      }
-    }
-  }
+			Expression r = stack.pop();
+			Expression l = stack.pop();
+			if (op == Operation.Operator.EQ) {
+				if (r instanceof IntVariable && l instanceof IntConstant) {
+					var = (IntVariable) r;
+					const1 = (IntConstant) l;
+				} else if (l instanceof IntVariable && r instanceof IntConstant) {
+					var = (IntVariable) l;
+					const1 = (IntConstant) r;
+				}
+				stack.push(new Operation(op, l, r));
+			} else {
+				if (r instanceof IntVariable) {
+					if (((IntVariable) r).getName().equals(var.getName())) {
+						r = const1;
+					}
+				}
+				if (l instanceof IntVariable) {
+					if (((IntVariable) l).getName().equals(var.getName())) {
+						l = const1;
+					}
+				}
+				stack.push(new Operation(op, l, r));
+			}
+		}
+	}
 }
